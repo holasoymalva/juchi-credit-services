05053310a8aa4c7cf706e2f723475c36df0f8a03
malvabombom <alquimista3301@gmail.com>
2024-04-14 11:54:49 -0600

    [feature] : Service Zillow Provider

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
 import torch
 
 app = Flask(__name__)
@@ -13,6 +14,13 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 MODEL_PATH = "./models/spam_classification_model.pth"
 model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
 model.eval()
+
+MODEL_PATH_RECOMMENDATIONS= "./models/recomendation.pth"
+tokenizerRecomendation = AutoTokenizer.from_pretrained(MODEL_NAME)
+modelRecomendation= AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=2)
+modelRecomendation.load_state_dict(torch.load(MODEL_PATH_RECOMMENDATIONS, map_location=torch.device('cpu')))
+modelRecomendation.eval()
+
 
 @app.route('/')
 def home():
@@ -39,6 +47,56 @@ def classify_text():
     # Puedes personalizar el retorno como desees
     classification = "spam" if predicted_class == 1 else "ham"
     return jsonify({"message": message, "classification": classification})
+
+@app.route('/magic-conch', methods=['POST'])
+def predict():
+    data = request.get_json()
+    sentence = data.get("sentence", "")
+
+    if not sentence:
+        return jsonify({"error": "No sentence provided"}), 400
+
+    inputs = tokenizer(sentence, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    with torch.no_grad():
+        outputs = modelRecomendation(**inputs)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1).numpy()
+    classification = "positive" if predictions[0] == 1 else "negative"
+
+    return jsonify({"sentence": sentence, "classification": classification})
+
+@app.route('/zillow/api/v2/zgecon/type')
+def get_data():
+    response_data = {
+        "success": True,
+        "status": 200,
+        "bundle": [
+            {"values": [], "description": "For-sale Inventory", "key": "invt_fs", "metadataType": "metricType"},
+            {"values": [], "description": "Mean Days to Pending", "key": "mean_doz_pending", "metadataType": "metricType"},
+            {"values": [], "description": "Zillow Home Value Index", "key": "zhvi", "metadataType": "metricType"}
+        ],
+        "total": 15
+    }
+    return jsonify(response_data)
+
+@app.route('/zillow/api/v2/zgecon/region')
+def get_region_type():
+    response_data = {
+        "success": True,
+        "status": 200,
+        "bundle": [
+            {"values": [], "description": "Country", "key": "1", "metadataType": "regionType"},
+            {"values": [], "description": "Metro", "key": "14", "metadataType": "regionType"},
+            {"values": [], "description": "State", "key": "2", "metadataType": "regionType"},
+            {"values": [], "description": "County", "key": "4", "metadataType": "regionType"},
+            {"values": [], "description": "City", "key": "6", "metadataType": "regionType"},
+            {"values": [], "description": "Zip", "key": "7", "metadataType": "regionType"},
+            {"values": [], "description": "Neighborhood", "key": "8", "metadataType": "regionType"}
+        ],
+        "total": 7
+    }
+    return jsonify(response_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
